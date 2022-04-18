d8080e248bcd7e8789aca76d1e60b2500e48caff
Viravich <viravich.phi@kkumail.com>
2022-04-18 14:36:08 +0700

    build delete button by id

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -3,12 +3,26 @@ import React, { Fragment, useState, useEffect } from "react";
 const ListTodo = () => {
   const [todos, setTodos] = useState([]);
 
+  //get all
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/get-all-todos");
       const jsonData = await response.json();
       setTodos(jsonData);
       // console.log(jsonData);
+    } catch (error) {
+      console.log({ message: error.message });
+    }
+  };
+
+  //delete by id
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodoById = await fetch(
+        `http://localhost:5000/delete-todo/${id}`,
+        { method: "DELETE" }
+      );
+      setTodos(todos.filter((data) => data.todo_id !== id));
     } catch (error) {
       console.log({ message: error.message });
     }
@@ -32,10 +46,17 @@ const ListTodo = () => {
         </thead>
         <tbody>
           {todos.map((data) => (
-            <tr>
+            <tr key={data.todo_id}>
               <td>{data.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(data.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
